a44f540133ebb7f88e8a2d6e9fcc879a55a39dcc
leva <lev.kireenko@inbox.ru>
2022-12-14 03:07:35 +0300

    add solution 1a

---
+++ conf.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Dense
+import tensorflow as tf
+from sklearn.metrics import roc_auc_score
+
+
+path_to_data = "static/worldcities.csv"
